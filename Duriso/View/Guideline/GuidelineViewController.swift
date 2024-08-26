cf366fb17a483f868d5b8134dc06de100dfd6bd3
shinsangkyu6660 <cherd3235@naver.com>
2024-08-26 14:21:55 +0900

    Design: UIlabel, imageView, collectionView creat
    Feat:UI몇개 생성 커밋

--- Duriso/View/Guideline/GuidelineViewController.swift
+++ Duriso/View/Guideline/GuidelineViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class GuidelineViewController: UIViewController {
-
+  
+  private let urgentMessage: UILabel = {
+    let label = UILabel()
+    label.text = "긴급재난문자"
+    label.font = .boldSystemFont(ofSize: 10)
+    return label
+  }()
+  
+  private let urgentMessageView: UILabel = {
+    let label = UILabel()
+    return label
+  }()
+  
+  private let disasterKit: UIImageView = {
+    let imageView = UIImageView()
+    imageView.layer.cornerRadius = 10
+    //imageView.image =
+    imageView.frame = .init(x: 0, y: 0, width: 100, height: 350)
+    return imageView
+  }()
+  
+  private lazy var atrickcollectionView: UICollectionView = {
+    let collectionView = UICollectionView()
+    collectionView.backgroundColor = .darkGray
+    collectionView.collectionViewLayout = .init()
+    return collectionView
+  }()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .systemBackground
+    guidelineLayout()
   }
-
-
+  
+  private func guidelineLayout() {
+    
+  }
+  
 }
 
+@available(iOS 17.0, *)
+#Preview{ GuidelineViewController() }
