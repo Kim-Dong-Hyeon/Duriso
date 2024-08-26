f217567408f7c61fb6275eecf4dab0b0990b6f05
jjooehee95 <leejh950417@naver.com>
2024-08-26 15:31:03 +0900

    Add: Font 파일 추가

--- Duriso/View/OnlineMap/OnlineMapViewController.swift
+++ Duriso/View/OnlineMap/OnlineMapViewController.swift
@@ -5,50 +5,20 @@
 //  Created by 김동현 on 8/26/24.
 //
 
+import KakaoMapsSDK
 import UIKit
 import SnapKit
 
 class OnlineMapViewController: UIViewController {
-  private let label: UILabel = {
-    let label = UILabel()
-    label.text = "우리동네 알리미"
-    label.font = CustomFont.Deco.font()
-    return label
-  }()
-  private let label2: UILabel = {
-    let label = UILabel()
-    label.text = "우리동네 알리미"
-    label.font = CustomFont.Display.font()
-    return label
-  }()
-  private let label3: UILabel = {
-    let label = UILabel()
-    label.text = "우리동네 알리미"
-    label.font = CustomFont.Deco4.font()
-    return label
-  }()
   
   override func viewDidLoad() {
-    super.viewDidLoad()
-    view.backgroundColor = .CBlue
-    view.addSubview(label)
-    view.addSubview(label2)
-    view.addSubview(label3)
-    
-    label.snp.makeConstraints { make in
-      make.center.equalToSuperview()
-    }
-    label2.snp.makeConstraints { make in
-      make.top.equalTo(label).offset(50)
-      make.centerX.equalToSuperview()
-    }
-    label3.snp.makeConstraints { make in
-      make.top.equalTo(label2).offset(50)
-      make.centerX.equalToSuperview()
-    }
-  }
+      super.viewDidLoad()
+      
+      }
 }
 
-@available(iOS 17.0, *)
-#Preview { OnlineMapViewController() }
+// @available(iOS 17.0, *)
+// #Preview { OnlineMapViewController() }
 
+
+  
