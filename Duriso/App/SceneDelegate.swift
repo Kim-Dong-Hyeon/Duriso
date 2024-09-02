5a64aeab2afda0d21c076280178be140ae58c514
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-09-02 21:24:01 +0900

    Feat: 네트워크 상태에 따른 페이지 구현

--- Duriso/App/SceneDelegate.swift
+++ Duriso/App/SceneDelegate.swift
@@ -11,52 +11,70 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
+  
+  /// 새로운 UIWindow를 생성하고 rootViewController를 설저
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+    // 이 메서드를 사용하면 제공된 UIWindowScene `scene`에 선택적으로 UIWindow `window`를 구성하고 첨부할 수 있습니다.
+    // 스토리보드를 사용하는 경우 `window` 프로퍼티가 자동으로 초기화되어 씬에 첨부됩니다.
+    // 이 델리게이트는 연결 장면이나 세션이 새로운 것을 의미하지 않습니다(대신 `application:configurationForConnectingSceneSession` 참조).
+    
     guard let windowScene = (scene as? UIWindowScene) else { return }
-    window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-    window?.windowScene = windowScene
+    
+    let window = UIWindow(windowScene: windowScene)
     
     // MARK: - 개발용 : 로그인 페이지 건너뛰고 MainTabBar로 바로 이동
-    window?.rootViewController = MainTabBarController()
     
-    // MARK: - 서비스용 : 로그인 페이지를 첫 화면으로 할 경우
+    let mainTabBarViewModel = MainTabBarViewModel()
+    let mainTabBarViewController = MainTabBarViewController(viewModel: mainTabBarViewModel)
+    
+    window.rootViewController = mainTabBarViewController
+    
+    // MARK: - 실 서비스용 : 로그인 페이지를 첫 화면으로 할 경우
+    
 //    let rootVC = LoginViewController()
 //    let introNavigationVC = UINavigationController(rootViewController: rootVC)
-//    self.window?.rootViewController = introNavigationVC
+//    window.rootViewController = introNavigationVC
     
-    window?.makeKeyAndVisible()
+    // MARK: - window 적용 부분
+    
+    window.makeKeyAndVisible()
+    
+    self.window = window
+    
+    /// 네트워크 모니터링 시작
+    NetworkMonitor.shared.startMonitoring()
   }
-
+  
+  /// scene이 연결 해제될 때 호출되는 메서드
   func sceneDidDisconnect(_ scene: UIScene) {
-    // Called as the scene is being released by the system.
-    // This occurs shortly after the scene enters the background, or when its session is discarded.
-    // Release any resources associated with this scene that can be re-created the next time the scene connects.
-    // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    // scene이 시스템에 의해 릴리스될 때 호출됩니다.
+    // scene이 백그라운드로 들어간 직후 또는 해당 세션이 삭제될 때 발생합니다.
+    // 다음에 scene이 연결될 때 다시 생성할 수 있는 이 scene과 관련된 모든 리소스를 해제합니다.
+    // 해당 세션이 반드시 삭제된 것은 아니므로 나중에 장면이 다시 연결될 수 있습니다(대신 `application:didDiscardSceneSessions` 참조).
+    
+    /// 네트워크 모니터링 중지
+    NetworkMonitor.shared.stopMonitoring()
   }
 
   func sceneDidBecomeActive(_ scene: UIScene) {
-    // Called when the scene has moved from an inactive state to an active state.
-    // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    // scene이 비활성 상태에서 활성 상태로 전환되었을 때 호출됩니다.
+    // scene이 비활성 상태일 때 일시 중지되었거나 아직 시작되지 않은 작업을 다시 시작하려면 이 메서드를 사용합니다.
   }
 
   func sceneWillResignActive(_ scene: UIScene) {
-    // Called when the scene will move from an active state to an inactive state.
-    // This may occur due to temporary interruptions (ex. an incoming phone call).
+    // scene이 활성 상태에서 비활성 상태로 전환될 때 호출됩니다.
+    // 일시적인 중단(예: 전화 수신)으로 인해 발생할 수 있습니다.
   }
 
   func sceneWillEnterForeground(_ scene: UIScene) {
-    // Called as the scene transitions from the background to the foreground.
-    // Use this method to undo the changes made on entering the background.
+    // scene이 배경에서 전경으로 전환될 때 호출됩니다.
+    // 배경에 진입할 때 변경한 내용을 취소하려면 이 메서드를 사용합니다.
   }
 
   func sceneDidEnterBackground(_ scene: UIScene) {
-    // Called as the scene transitions from the foreground to the background.
-    // Use this method to save data, release shared resources, and store enough scene-specific state information
-    // to restore the scene back to its current state.
+    // scene이 전경에서 배경으로 전환될 때 호출됩니다.
+    // 이 메서드를 사용하면 데이터를 저장하고, 공유 리소스를 해제하고, scene별 상태 정보를 충분히 저장할 수 있습니다.
+    // scene을 현재 상태로 복원할 수 있습니다.
   }
 
 
