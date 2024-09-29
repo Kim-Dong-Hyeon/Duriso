da6abcebcb66d267fc9b61492acbf193f1ab72f3
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-09-29 10:57:45 +0900

    Feat: POIType 구현

---
+++ Duriso/Model/OfflineMap/POIType.swift
@@ -0,0 +1,15 @@
+//
+//  POIType.swift
+//  Duriso
+//
+//  Created by 김동현 on 9/28/24.
+//
+
+import Foundation
+import MapLibre
+
+enum POIType: String {
+  case aed
+  case civilDefenseShelter
+  case disasterShelter
+}
