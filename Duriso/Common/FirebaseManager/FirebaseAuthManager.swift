df2c328e27cb05e24ab8f1690b36056a5a4f86a3
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-10-13 07:49:44 +0900

    Feat: FirebaseAuthManager 분리 및 싱글톤 구현

--- Duriso/Common/FirebaseManager/FirebaseAuthManager.swift
+++ Duriso/Common/FirebaseManager/FirebaseAuthManager.swift
@@ -5,4 +5,55 @@
 //  Created by 김동현 on 10/10/24.
 //
 
-import Foundation
+import FirebaseAuth
+import RxSwift
+
+class FirebaseAuthManager {
+  static let shared = FirebaseAuthManager()
+  private init() {}
+  
+  func signIn(withEmail email: String, password: String) -> Observable<AuthDataResult> {
+    return Observable.create { observer in
+      Auth.auth().signIn(withEmail: email, password: password) { result, error in
+        if let error = error {
+          observer.onError(error)
+        } else if let result = result {
+          observer.onNext(result)
+          observer.onCompleted()
+        }
+      }
+      return Disposables.create()
+    }
+  }
+  
+  func createUser(withEmail email: String, password: String) -> Observable<AuthDataResult> {
+    return Observable.create { observer in
+      Auth.auth().createUser(withEmail: email, password: password) { result, error in
+        if let error = error {
+          observer.onError(error)
+        } else if let result = result {
+          observer.onNext(result)
+          observer.onCompleted()
+        }
+      }
+      return Disposables.create()
+    }
+  }
+  
+  func signOut() -> Observable<Void> {
+    return Observable.create { observer in
+      do {
+        try Auth.auth().signOut()
+        observer.onNext(())
+        observer.onCompleted()
+      } catch let error {
+        observer.onError(error)
+      }
+      return Disposables.create()
+    }
+  }
+  
+  func currentUser() -> FirebaseAuth.User? {
+    return Auth.auth().currentUser
+  }
+}
