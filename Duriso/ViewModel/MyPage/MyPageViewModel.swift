a76c9ed9acd448a4603a729478885d93dbc5450e
Kim-Dong-Hyeon <dhkim960206@gmail.com>
2024-10-25 02:45:32 +0900

    Refactor: MyPage 관련 로직 MVVM 리펙토링

--- Duriso/ViewModel/MyPage/MyPageViewModel.swift
+++ Duriso/ViewModel/MyPage/MyPageViewModel.swift
@@ -5,22 +5,30 @@
 //  Created by 김동현 on 8/26/24.
 //
 
+import AuthenticationServices
+
 import FirebaseAuth
-import FirebaseFirestore
+import RxCocoa
 import RxSwift
 
-class MyPageViewModel {
+class MyPageViewModel: NSObject {
+  // Inputs
+  let logoutTrigger = PublishSubject<Void>()
+  let deleteAccountTrigger = PublishSubject<Void>()
+  
+  // Outputs
   let items: Observable<[MyPageModel]>
+  let nickname = BehaviorRelay<String>(value: "")
+  let postcount = BehaviorRelay<Int>(value: 0)
+  let logoutResult = PublishSubject<Result<Void, Error>>()
+  let deleteAccountResult = PublishSubject<Result<Void, Error>>()
   
-  let nickname: BehaviorSubject<String> = BehaviorSubject(value: "")
-  let postcount: BehaviorSubject<Int> = BehaviorSubject(value: 0)
-  
-  private let firestore = Firestore.firestore()
   private let disposeBag = DisposeBag()
+  private var accountDeletionObserver: AnyObserver<Void>?
   
-  init() {
+  override init() {
     let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "Unknown"
-    //앱의 버전을 앱의 설정파일에서 불러옴 (빌드나 배포에 따라 버전 정보가 자동 업데이트)
+    // 앱의 버전을 앱의 설정파일에서 불러옴 (빌드나 배포에 따라 버전 정보가 자동 업데이트)
     
     items = Observable.just([
       MyPageModel(title: "차단 목록", type: .disclosure, selected: true),
@@ -32,35 +40,178 @@ class MyPageViewModel {
       MyPageModel(title: "로그아웃", type: .disclosure, selected: true),
       MyPageModel(title: "회원탈퇴", type: .disclosure, selected: true)
     ])
+    super.init()
     
+    setupBindings()
     fetchUserData()
   }
   
+  private func setupBindings() {
+    logoutTrigger
+      .flatMapLatest { FirebaseAuthManager.shared.signOut() }
+      .subscribe(onNext: { [weak self] in
+        self?.logoutResult.onNext(.success(()))
+      }, onError: { [weak self] error in
+        self?.logoutResult.onNext(.failure(error))
+      })
+      .disposed(by: disposeBag)
+    
+    deleteAccountTrigger
+      .do(onNext: { print("회원탈퇴 프로세스 시작") })
+      .flatMapLatest { [weak self] in
+        print("사용자 재인증 시작")
+        return self?.reauthenticateUser() ?? Observable.error(NSError(domain: "ReauthError", code: -1, userInfo: nil))
+      }
+      .do(onNext: { print("사용자 재인증 성공") })
+      .flatMapLatest {
+        print("계정 삭제 시작")
+        return FirebaseAuthManager.shared.deleteAccount()
+      }
+      .subscribe(
+        onNext: { [weak self] in
+          print("계정 삭제 성공")
+          self?.deleteAccountResult.onNext(.success(()))
+        },
+        onError: { [weak self] error in
+          print("계정 삭제 실패: \(error.localizedDescription)")
+          self?.deleteAccountResult.onNext(.failure(error))
+        }
+      )
+      .disposed(by: disposeBag)
+  }
+  
   private func fetchUserData() {
+    guard let uid = FirebaseAuthManager.shared.getCurrentUser()?.uid else { return }
     
-    guard let user = FirebaseAuthManager.shared.currentUser() else { return }
-    
-    let uid = user.uid
-    
-//    let safeEmail = user.email?.replacingOccurrences(of: ".", with: "-") ?? ""
-    firestore.collection("users").document(uid).getDocument { [weak self] (document, error) in
-      guard let self = self else { return }
-      if let document = document, document.exists {
-        let data = document.data()
-        let nickname = data?["nickname"] as? String ?? ""
-        let postcount = data?["postcount"] as? Int ?? 0
-        
-        // 닉네임과 게시글 수 업데이트
-        self.nickname.onNext(nickname)
-        self.postcount.onNext(postcount)
+    FirebaseFirestoreManager.shared.fetchUserData(uid: uid)
+      .subscribe(onNext: { [weak self] data in
+        self?.nickname.accept(data["nickname"] as? String ?? "")
+        self?.postcount.accept(data["postcount"] as? Int ?? 0)
+      }, onError: { error in
+        print("Failed to fetch user data: \(error)")
+      })
+      .disposed(by: disposeBag)
+  }
+  
+  // 사용자 재인증 메서드
+  private func reauthenticateUser() -> Observable<Void> {
+    return Observable.create { [weak self] observer in
+      guard let self = self else {
+        observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Self is nil"]))
+        return Disposables.create()
+      }
+      
+      guard let user = Auth.auth().currentUser else {
+        observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "No current user"]))
+        return Disposables.create()
+      }
+      
+      if let providerData = user.providerData.first {
+        switch providerData.providerID {
+        case "password":
+          self.reauthenticateWithPassword(observer: observer)
+        case "apple.com":
+          self.reauthenticateWithApple(observer: observer)
+        default:
+          observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Unsupported provider"]))
+        }
       } else {
-        print("사용자 데이터를 불러오는 데 실패했습니다: \(error?.localizedDescription ?? "")")
+        observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "No provider data"]))
+      }
+      
+      return Disposables.create()
+    }
+  }
+  
+  private func reauthenticateWithPassword(observer: AnyObserver<Void>) {
+    let alert = UIAlertController(title: "재인증", message: "계정 삭제를 위해 비밀번호를 다시 입력해주세요.", preferredStyle: .alert)
+    alert.addTextField { textField in
+      textField.placeholder = "비밀번호"
+      textField.isSecureTextEntry = true
+    }
+    alert.addAction(UIAlertAction(title: "확인", style: .default, handler: { [weak self] _ in
+      guard let password = alert.textFields?.first?.text, !password.isEmpty else {
+        observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Password is empty"]))
+        return
+      }
+      
+      guard let email = Auth.auth().currentUser?.email else {
+        observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "No email found"]))
+        return
+      }
+      
+      let credential = EmailAuthProvider.credential(withEmail: email, password: password)
+      Auth.auth().currentUser?.reauthenticate(with: credential) { _, error in
+        if let error = error {
+          observer.onError(error)
+        } else {
+          observer.onNext(())
+          observer.onCompleted()
+        }
+      }
+    }))
+    alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: { _ in
+      observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "Reauthentication cancelled"]))
+    }))
+    
+    DispatchQueue.main.async {
+      UIApplication.shared.windows.first?.rootViewController?.present(alert, animated: true, completion: nil)
+    }
+  }
+  
+  private func reauthenticateWithApple(observer: AnyObserver<Void>) {
+    guard let user = Auth.auth().currentUser else {
+      observer.onError(NSError(domain: "ReauthError", code: -1, userInfo: [NSLocalizedDescriptionKey: "No current user"]))
+      return
+    }
+    
+    let appleIDProvider = ASAuthorizationAppleIDProvider()
+    let request = appleIDProvider.createRequest()
+    request.requestedScopes = [.fullName, .email]
+    request.user = user.uid // 현재 사용자 UID 설정
+    
+    let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+    authorizationController.delegate = self
+    authorizationController.presentationContextProvider = self
+    authorizationController.performRequests()
+    
+    NotificationCenter.default.addObserver(forName: .appleSignInCompleted, object: nil, queue: nil) { notification in
+      if let error = notification.userInfo?["error"] as? Error {
+        observer.onError(error)
+      } else {
+        observer.onNext(())
+        observer.onCompleted()
+      }
+    }
+  }
+}
+
+extension MyPageViewModel: ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
+  func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+    return UIApplication.shared.windows.first!
+  }
+  
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+    if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential,
+       let appleIDToken = appleIDCredential.identityToken,
+       let idTokenString = String(data: appleIDToken, encoding: .utf8) {
+      
+      let credential = OAuthProvider.credential(withProviderID: "apple.com", idToken: idTokenString, rawNonce: "")
+      
+      Auth.auth().currentUser?.reauthenticate(with: credential) { _, error in
+        if let error = error as NSError? {
+          print("Reauthentication failed: \(error.localizedDescription), \(error.userInfo)")
+          NotificationCenter.default.post(name: .appleSignInCompleted, object: nil, userInfo: ["error": error])
+        } else {
+          print("Reauthentication successful")
+          NotificationCenter.default.post(name: .appleSignInCompleted, object: nil)
+        }
       }
     }
   }
   
-  // 로그아웃 메서드
-  func logout() -> Observable<Void> {
-    return FirebaseAuthManager.shared.signOut()
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+    print("Authorization error: \(error.localizedDescription)")
+    NotificationCenter.default.post(name: .appleSignInCompleted, object: nil, userInfo: ["error": error])
   }
 }
